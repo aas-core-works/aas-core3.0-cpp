4cc0f6514d995ce6d6e787b6388a0cf2d7d38e5e
Marko Ristin <marko.ristin@gmail.com>
2024-02-10 18:39:00 +0100

    Fix and test jsonization of concrete classes (#14)
    We generate the tests related to jsonization of concrete classes, and
    fix the code and/or code generator in aas-core-codegen where necessary.

---
+++ test/common.hpp
@@ -0,0 +1,71 @@
+#ifndef TEST_COMMON_
+#define TEST_COMMON_
+
+#include <aas_core/aas_3_0/types.hpp>
+
+#include <nlohmann/json.hpp>
+
+#include <deque>
+#include <filesystem>
+
+/**
+* Provide common functionalities shared across the tests.
+*/
+namespace test {
+namespace common {
+
+/**
+ * Set to true if we run tests in the record mode according to the environment
+ * variable `AAS_CORE_3_0_CPP_RECORD_MODE`.
+ *
+ * \remark It is tedious to record manually all the expected error messages. Therefore
+ * we include this variable to steer the automatic recording. We intentionally
+ * intertwine the recording code with the test code to keep them close to each other
+ * so that they are easier to maintain.
+ */
+bool DetermineRecordMode();
+
+/**
+ * Point to the directory with the test data according to the environment
+ * variable `AAS_CORE_3_0_CPP_TEST_DATA_DIR`.
+ */
+std::filesystem::path DetermineTestDataDir();
+
+/**
+ * Read the content of the `path` and parse it as JSON.
+ */
+nlohmann::json MustReadJson(const std::filesystem::path& path);
+
+/**
+ * Represent `that` instance as a human-readable line of an iteration trace.
+ */
+std::wstring TraceMark(
+  const aas_core::aas_3_0::types::IClass& that
+);
+
+/**
+ * Find files beneath the `root` recursively which have the `suffix`.
+ *
+ * If the `root` does not exist, return an empty deque.
+ *
+ * If the `root` is not a directory, throw a runtime error.
+ *
+ * The files are sorted.
+ */
+std::deque<std::filesystem::path> FindFilesBySuffixRecursively(
+  const std::filesystem::path& root,
+  const std::string& suffix
+);
+
+/**
+ * Check that the content coincides with the file or re-record if in record mode.
+ */
+void AssertContentEqualsExpectedOrRecord(
+  const std::string& content,
+  const std::filesystem::path& path
+);
+
+}  // namespace common
+}  // namespace test
+
+#endif
---
+++ test/common_jsonization.hpp
@@ -0,0 +1,34 @@
+/**
+ * Provide methods which are used throughout the jsonization tests.
+ */
+#ifndef AAS_CORE3_TEST_COMMON_JSONIZATION_H_
+#define AAS_CORE3_TEST_COMMON_JSONIZATION_H_
+
+#include <nlohmann/json.hpp>
+
+#include <optional>
+#include <vector>
+#include <string>
+
+namespace test {
+namespace common {
+namespace jsonization {
+
+extern const std::vector<std::string> kCausesForDeserializationFailure;
+
+/**
+ * Compare two JSON values.
+ *
+ * Return the JSON patch, if there is any difference.
+ */
+std::optional<std::string> CompareJsons(
+  const nlohmann::json& that,
+  const nlohmann::json& other
+);
+
+}  // namespace jsonization
+}  // namespace common
+}  // namespace test
+
+
+#endif //AAS_CORE3_TEST_COMMON_JSONIZATION_H_
