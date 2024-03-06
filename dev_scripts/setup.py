8f78d850ae862c7405a731c14681cd5a32510925
Marko Ristin <marko.ristin@gmail.com>
2024-03-06 19:48:28 +0100

    Update to aas-core-meta, codegen, testgen 79314c6, a206fe8, 069db38b
    We update the development requirements to and re-generate everything
    with:
    * [aas-core-meta 79314c6],
    * [aas-core-codegen a206fe8] and
    * [aas-core3.0-testgen 069db38b].
    
    Notably, we propagate the fix to the patterns of `xs:date` and
    `xs:dateTime`, since the previous patterns allowed for an incorrect
    offset suffix without a sign (`14:00` could be simply concatenated).
    
    [aas-core-meta 79314c6]: https://github.com/aas-core-works/aas-core-meta/commit/79314c6
    [aas-core-codegen a206fe8]: https://github.com/aas-core-works/aas-core-codegen/commit/a206fe8
    [aas-core3.0-testgen 069db38b]: https://github.com/aas-core-works/aas-core3.0-testgen/commit/069db38b

--- dev_scripts/setup.py
+++ dev_scripts/setup.py
@@ -29,8 +29,8 @@ setup(
     keywords="asset administration shell code generation industry 4.0 industrie i4.0",
     install_requires=[
         "icontract>=2.6.1,<3",
-        "aas-core-meta@git+https://github.com/aas-core-works/aas-core-meta@c9692bc#egg=aas-core-meta",
-        "aas-core-codegen@git+https://github.com/aas-core-works/aas-core-codegen@256cc8a#egg=aas-core-codegen",
+        "aas-core-meta@git+https://github.com/aas-core-works/aas-core-meta@79314c6#egg=aas-core-meta",
+        "aas-core-codegen@git+https://github.com/aas-core-works/aas-core-codegen@a206fe8#egg=aas-core-codegen",
     ],
     # fmt: off
     extras_require={
