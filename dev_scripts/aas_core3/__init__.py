481e680d7f51a68c1dd3412471acac8249486b26
Marko Ristin <marko.ristin@gmail.com>
2024-03-07 17:04:41 +0100

    Update to aas-core-meta, codegen, testgen 79314c6, e8d050c, e1087880 (#32)
    We update the development requirements to and re-generate everything
    with:
    * [aas-core-meta 79314c6],
    * [aas-core-codegen e8d050c] and
    * [aas-core3.0-testgen e1087880].
    
    Notably, we propagate the changes to test data from testgen related to positive
    examples of `xs:double`, which were previously out-of-range.
    
    [aas-core-meta 79314c6]: https://github.com/aas-core-works/aas-core-meta/commit/79314c6
    [aas-core-codegen e8d050c]: https://github.com/aas-core-works/aas-core-codegen/commit/e8d050c
    [aas-core3.0-testgen e1087880]: https://github.com/aas-core-works/aas-core3.0-testgen/commit/e1087880

--- dev_scripts/aas_core3/__init__.py
+++ dev_scripts/aas_core3/__init__.py
@@ -3,5 +3,5 @@ Provide Python SDK as copied from aas-core-codegen test data.
 
 This copy is necessary so that we can decouple from ``aas-core*-python`` repository.
 
-The revision of aas-core-codegen was: e90f027
+The revision of aas-core-codegen was: e8d050c
 """
