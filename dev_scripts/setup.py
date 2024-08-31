08f943229c18555004489bb20a2f550e8fc71520
Marko Ristin <marko.ristin@gmail.com>
2024-08-31 16:19:02 +0500

    Update to aas-core-meta, codegen, testgen f9cbdb3, b14237b2, 5a705a0b1
    We update the development requirements to and re-generate everything
    with:
    * [aas-core-meta f9cbdb3],
    * [aas-core-codegen b14237b2] and
    * [aas-core3.0-testgen 5a705a0b1].
    
    Notably, we propagate the fix for AASd-109 where we change the invariant
    such that it checks for the consistency among properties even when
    a ``value`` property is missing.
    
    [aas-core-meta f9cbdb3]: https://github.com/aas-core-works/aas-core-meta/commit/f9cbdb3
    [aas-core-codegen b14237b2]: https://github.com/aas-core-works/aas-core-codegen/commit/b14237b2
    [aas-core3.0-testgen 5a705a0b1]: https://github.com/aas-core-works/aas-core3.0-testgen/commit/5a705a0b1

--- dev_scripts/setup.py
+++ dev_scripts/setup.py
@@ -29,8 +29,8 @@ setup(
     keywords="asset administration shell code generation industry 4.0 industrie i4.0",
     install_requires=[
         "icontract>=2.6.1,<3",
-        "aas-core-meta@git+https://github.com/aas-core-works/aas-core-meta@31d6afd#egg=aas-core-meta",
-        "aas-core-codegen@git+https://github.com/aas-core-works/aas-core-codegen@0f7345e1#egg=aas-core-codegen",
+        "aas-core-meta@git+https://github.com/aas-core-works/aas-core-meta@f9cbdb3#egg=aas-core-meta",
+        "aas-core-codegen@git+https://github.com/aas-core-works/aas-core-codegen@b14237b2#egg=aas-core-codegen",
     ],
     # fmt: off
     extras_require={
